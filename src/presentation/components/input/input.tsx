861936248092832bddc2363a072c923dc4efc6bd
Daniel <daniel.n10@hotmail.com>
2021-04-11 19:45:39 -0300

    feat: ensure Login start with inputs with required start with required state

--- src/presentation/components/input/input.tsx
+++ src/presentation/components/input/input.tsx
@@ -1,13 +1,22 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import Styles from './input-styles.scss'
+import Context from '@/presentation/contexts/form/form-context'
 
 type Props = React.DetailedHTMLProps<React.InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>
 
 const Input: React.FC<Props> = (props: Props) => {
+  const { errorState } = useContext(Context)
+  const error = errorState[props.name]
+  const getStatus = (): string => {
+    return '🔴'
+  }
+  const getTitle = (): string => {
+    return error
+  }
   return (
     <div className={Styles.inputWrap}>
       <input {...props} />
-      <span className={Styles.status}>🔴</span>
+      <span data-testid={`${props.name}-status`} title={getTitle()} className={Styles.status}>{getStatus()}</span>
     </div>
   )
 }
