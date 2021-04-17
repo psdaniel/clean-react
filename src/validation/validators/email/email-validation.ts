fba760b246e3f162b23f7d7eafcb0c1331921eb8
Daniel <daniel.n10@hotmail.com>
2021-04-17 18:49:20 -0300

    refactor: move SUT creation to a factory method

--- src/validation/validators/email/email-validation.ts
+++ src/validation/validators/email/email-validation.ts
@@ -1,6 +1,5 @@
 import { InvalidFieldError } from '@/validation/errors'
 import { FieldValidation } from '@/validation/protocols/field-validation'
-
 export class EmailValidation implements FieldValidation {
   constructor (readonly field: string) {}
 
